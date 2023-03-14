f7edda68937ff2869355f2feba217daf8c00f9b5
Jack Beatty <91834919+Jack-Beatty@users.noreply.github.com>
2023-03-13 19:47:19 -0500

    Creating the ClawRotation enum and subsystem

---
+++ TeamCode/src/main/java/edu/edina/library/subsystems/Claw.java
@@ -0,0 +1,27 @@
+package edu.edina.library.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+import java.sql.Time;
+
+import edu.edina.library.util.ClawServoPosition;
+import edu.edina.library.util.ArmServoPosition;
+import edu.edina.library.util.PoleLocation;
+import edu.edina.library.util.RobotState;
+import edu.edina.library.util.ClawRotation;
+import edu.edina.library.util.Stickygamepad;
+
+public class Claw extends edu.edina.library.subsystems.Subsystem {
+    private Stickygamepad _gamepad2;
+
+
+    @Override
+    public void update() {
+
+    }
+}
